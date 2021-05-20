e2b86213bebc19cdc68f744ad4baa8518d1325ae
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-20 16:31:38 +0200

    inizio

---
+++ src/components/Header.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="wrap">
+    <div class="box80">
+    <div class="logo">
+      <img src="@/assets/img/dc-logo.png" alt="">
+    </div>
+    <div class="menu">
+      <ul>
+        <li
+        v-for="(link ,index) in links" :key="index">
+        <a >{{link.nome}}</a>
+        <div class="quadrato"></div>
+        </li>
+      </ul>
+    </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return{
+    links:[
+
+      {
+        nome: 'characters',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'comics',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'movies',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'tv',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'games',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'collectibles',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'videos',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'fans',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'news',
+        url:'/',
+        flag: false      
+      },
+      {
+        nome: 'shop',
+        url:'/',
+        flag: false      
+      }
+    ] 
+    }
+
+  }
+}
+</script> 
+
+<style lang='scss' scoped>
+.wrap{
+  height: 120px;
+  background-color: red;
+
+  .box80{
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+
+  .logo{
+    width: 40%;
+    img{
+      height: 80px;
+  }
+  }
+  .menu{
+    width: 70%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    ul{
+      list-style: none;
+
+      li{
+        display: inline-block;
+        margin: 10px;
+        font-size: 11px;
+        text-transform: uppercase;
+        height: 120px;
+        line-height: 120px;
+        position: relative;
+        
+        a{
+          transition: background 0.4s;
+          &:hover,
+          &.active{
+          color: #0282f9;
+          }
+          &:hover .menu ul li .quadrato{
+          display: block;
+          }
+        }
+
+        .quadrato{
+          /* display: none; */
+          position: absolute;
+          bottom: 0;
+          height: 5px;
+          width: 100%;
+          background-color: #0282f9;
+        }
+      }
+    }
+  }
+}
+}
+</style>
