1f25f0614b51792503bdc729573925552881252b
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-20 17:05:38 +0200

    header done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -83,12 +83,14 @@ export default {
 </script> 
 
 <style lang='scss' scoped>
+@import '@/assets/style/general.scss';
+
 .wrap{
   height: 120px;
-  background-color: red;
+  /* background-color: red; */
 
   .box80{
-  width: 80%;
+  width: $box80; //$box80
   height: 100%;
   margin: auto;
   padding: 10px;
@@ -118,25 +120,23 @@ export default {
         height: 120px;
         line-height: 120px;
         position: relative;
-        
-        a{
-          transition: background 0.4s;
-          &:hover,
-          &.active{
-          color: #0282f9;
-          }
-          &:hover .menu ul li .quadrato{
-          display: block;
-          }
+        cursor: pointer;
+        transition: background 0.4s;
+        color: $colorTextGrey;
+        &:hover,
+        &.active{
+        color: $colorTextBlue;
         }
-
         .quadrato{
-          /* display: none; */
+          display: none;
           position: absolute;
           bottom: 0;
           height: 5px;
           width: 100%;
-          background-color: #0282f9;
+          background-color: $colorTextBlue;
+        }
+        &:hover .quadrato{
+          display: block;
         }
       }
     }
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <h1>dio porco</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style>
+
+</style>
